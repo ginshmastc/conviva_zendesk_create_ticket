343f4a8a654d62516bc132fb0075ff3a29d2d129
Erik <erik_holden@comcast.net>
2019-01-30 11:33:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,4 @@ def handle_form():
         print(status)
     return template('ticket_form', feedback=status, no_email=ask_email)
 
-if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-    run(host='localhost', port=8080, debug=True)
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
