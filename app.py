8f365cc302385519a5d7eb2a3057035b2f57c81b
Erik <erik_holden@comcast.net>
2019-02-01 12:08:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,14 @@
-import json
 import os
-import requests
-from bottle import route, template, run, static_file, request, response
+from bottle import route, run
+
+@route('/hello/:name')
+def index(name='World'):
+    return '<b>Hello %s!</b>' % name
 
 
+if __name__ == '__main__':
+    # Get required port, default to 5000.
+    port = os.environ.get('PORT', 5000)
 
-@route('/', method=['GET', 'POST'])
-def handle_form():
-    if 'verified_email' in request.cookies:
-        ask_email = False
-    else:
-        ask_email = True
-    status = ''
-
-    if request.POST:
-        # Get the form data
-        subject = request.forms.get('subject')
-        description = request.forms.get('description')
-        if 'verified_email' in request.cookies:
-            email = request.get_cookie('verified_email')
-        else:
-            email = request.forms.get('email')
-        # Package the data for the API
-        data = {'request': {'subject': subject, 'comment': {'body': description}}}
-        ticket = json.dumps(data)
-        print(ticket)
-        # Make the API request
-        user = email + '/token'
-        api_token = 'FOV7zAS4ZIGylDLyqr4LbA56VNXgB9FFIU5hXkXF'
-        url = 'https://convivasupport.zendesk.com/api/v2/requests.json'
-        headers = {'content-type': 'application/json'}
-        r = requests.post(url, data=ticket, auth=(user, api_token), headers=headers)
-        if r.status_code != 201:
-            if r.status_code == 401 or 422:
-                status = 'Could not authenticate you. Check your email address or register.'
-                ask_email = True
-            else:
-                status = 'Problem with the request. Status ' + str(r.status_code)
-        else:
-            status = 'Ticket was created. Look for an email notification.'
-            if 'verified_email' not in request.cookies:
-                response.set_cookie('verified_email', email, max_age=364*24*3600)
-                ask_email = False
-        print(status)
-    return template('ticket_form', feedback=status, no_email=ask_email)
-port = os.environ.get('PORT', 5000)
-print('port ' + str(port))
-#if os.environ.get('APP_LOCATION') == 'heroku':
-run(host="0.0.0.0", port=port)
-#else:
-#    run(host='localhost', port=8080, debug=True)
+    # Run the app.
+run(host='0.0.0.0', port=port)
